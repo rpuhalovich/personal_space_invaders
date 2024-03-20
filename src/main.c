053c7406d4589c564296058d2f0240fc4d65e362
Ryan Puhalovich <reanpuhalovich@gmail.com>
2024-03-20 20:29:51 +1100

    Implement bouncing back and forth

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <raylib.h>
 #include <raymath.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 #include "types.h"
@@ -29,6 +30,9 @@ typedef struct {
     struct {
         Alien alienGrid[ALIEN_ROWS][ALIEN_COLS];
         i32 speed;
+        f32 alienOffset;
+        bool isLeft;
+        f32 yOffset;
     } aliens;
 } State;
 State state = {0};
@@ -39,21 +43,23 @@ void printv2(Vector2 v) {
 
 void init() {
     i32 scaleFactor = 3;
-    State s = {.time = 0,
-               .window =
-                   {
-                       .width = 224 * scaleFactor,
-                       .height = 256 * scaleFactor,
-                       .scaleFactor = scaleFactor,
-                       .targetFps = 60,
-                   },
-               .ship =
-                   {
-                       .speed = 100.f * scaleFactor,
-                   },
-               .aliens = {
-                   .alienGrid = {0},
-               }};
+    State s = {
+        .time = 0,
+        .window = {
+               .width = 224 * scaleFactor,
+               .height = 256 * scaleFactor,
+               .scaleFactor = scaleFactor,
+               .targetFps = 60,
+           },
+           .ship = {
+               .speed = 100.f * scaleFactor,
+           },
+           .aliens = {
+               .alienGrid = {0},
+               .speed = 80.f,
+               .isLeft = false,
+           }
+    };
     state = s;
 
     InitWindow(s.window.width, s.window.height, "Personal Space Invaders");
@@ -65,9 +71,7 @@ void init() {
         state.ship.texture = LoadTextureFromImage(img);
         UnloadImage(img);
 
-        Vector2 pos = {5.f * scaleFactor,
-                       state.window.height -
-                           scaleFactor * (state.ship.texture.height + 5.f)};
+        Vector2 pos = {5.f * scaleFactor, state.window.height - scaleFactor * (state.ship.texture.height + 5.f)};
         state.ship.pos = pos;
     }
 
@@ -102,19 +106,37 @@ void update() {
     }
 
     // draw ship
-    DrawTextureEx(state.ship.texture, state.ship.pos, 0, sf, WHITE);
+    {
+        DrawTextureEx(state.ship.texture, state.ship.pos, 0, sf, WHITE);
+    }
 
     // draw aliens
     {
+        f32 dir = state.aliens.isLeft ? -1 : 1;
+        state.aliens.speed = abs(state.aliens.speed) * dir;
+        state.aliens.alienOffset += state.aliens.speed * GetFrameTime();
+
         f32 spacing = 5.f, offset = 20.f;
         for (int r = 0; r < ALIEN_ROWS; r++) {
             for (int c = 0; c < ALIEN_COLS; c++) {
                 Alien a = state.aliens.alienGrid[r][c];
                 Vector2 newpos = {(a.texture.width + spacing) * c * sf, (a.texture.height + spacing) * r * sf};
                 newpos.y += offset * 3;
+                newpos.y += state.aliens.yOffset;
                 newpos.x += offset;
-                a.pos = newpos;
+                newpos.x += state.aliens.alienOffset;
+                state.aliens.alienGrid[r][c].pos = newpos;
                 DrawTextureEx(a.texture, a.pos, 0, sf, WHITE);
+            }
+        }
+
+        for (int r = 0; r < ALIEN_ROWS; r++) {
+            for (int c = 0; c < ALIEN_COLS; c++) {
+                Vector2 pos = state.aliens.alienGrid[r][c].pos;
+                if (pos.x > state.window.width - state.ship.texture.width * sf - 20.f || pos.x <= 0.f) {
+                    state.aliens.isLeft = !state.aliens.isLeft;
+                    state.aliens.yOffset += state.aliens.alienGrid[r][c].texture.height;
+                }
             }
         }
     }
